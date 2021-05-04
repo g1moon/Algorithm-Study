776f7729ed8df4983fc216ea027ae87423b34130
g1moon <gmoon.cho@gmail.com>
2021-05-04 16:53:25 +0900

    Add 카카오기출 길찾기게임 성공

--- gimoon/kakao_길찾기게임.py
+++ gimoon/kakao_길찾기게임.py
@@ -1,20 +1,44 @@
 import copy 
-'''
-- 두 팀으로 나누고 
-- 같은 곳을 다른 순서로 방문 -> 먼저 순회를 마치면 승리 
-- 방문할 곳의 2차원 좌표 값을 구하고 -> 각 장소를 이진트리의 노드가 되도록 구성하고 
-    -> 순회 방법을 힌트로 줘 -> 각 팀이 스스로 경로 찾게 
+import sys
+sys.setrecursionlimit(10**6) #설정해주지 않으면 런타임 에러가 난다.
 
-- 서로 다른 x값
-- 같은 레벨에 있는 노드는 같은 y좌표 
-- 자식은 항상 y값이 작아 
-- 같은 레벨에서 x값은 오름차순
-- 
 '''
-nodeinfo = [[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
-result = [[7,4,6,9,1,8,5,2,3],[9,6,5,8,1,4,3,2,7]]
+- 넣을때 루트 노드부터 비교하면서 x값이 작으면 -> 왼쪽, 크면-> 오른쪽
+- False(비어있으면) -> 넣어주고, False가 아니면(값이 있으면) -> 
+    -> 그 값이 비교 대상이 된다. 
+    -> 위 작업을 루프 돌리고 -> 넣어주면 종료
+'''
+
+def firDfs(nodeId):
+    if dic[nodeId][2]:
+        _id = dic[nodeId][2]
+        firDfs(_id)
+        
+    if dic[nodeId][3]:
+        _id = dic[nodeId][3]
+        firDfs(_id)
+    
+    lst1.append(nodeId) 
+    return 
+        
+def secDfs(nodeId):
+    
+    if nodeId not in lst2:
+        lst2.append(nodeId)
+        
+    if dic[nodeId][2]:
+        _id = dic[nodeId][2]
+        secDfs(_id)
+        
+    if dic[nodeId][3]:
+        _id = dic[nodeId][3]
+        secDfs(_id)
+        
+    return     
 
 def solution(nodeinfo):
+    global dic, lst1, lst2
+    lst1, lst2 = [], []
     #dic{id : [x, y, l, r]}
     dic = {}
     ans = [[]]
@@ -58,12 +82,9 @@ def solution(nodeinfo):
                     comNodeId = copy.deepcopy(comNodeR)
                     continue
                     
-    print(dic)
-
-    return ans
-print(solution(nodeinfo))
-
-        
-        
-        
+    #여기까지 오면 dic만들어짐-------------
+    firDfs(rootId)
+    secDfs(rootId)
+    ans = [lst2, lst1]
     
+    return ans
